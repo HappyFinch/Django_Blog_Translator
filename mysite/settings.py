6d53660ff9f9da746e80ec864c6ff47d25a9c340
HQY <13191099631@163.com>
2022-11-17 15:50:57 +0800

    把我的blog app 加入到setting里

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',   # add my app into this list!
 ]
 
 MIDDLEWARE = [
